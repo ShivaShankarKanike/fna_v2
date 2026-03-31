a192e098ad3d0034067a7df364b2ed6216cc1085
Shiva Shankar Kanike <149442909+ShivaShankarKanike@users.noreply.github.com>
2026-03-31 03:02:20 -0400

    Add FNAMemoryLayer and update __all__ exports

--- fna/__init__.py
+++ fna/__init__.py
@@ -2,3 +2,10 @@ from .fluid_dynamics import advect, diffuse, project, ns_step
 from .adapter import FNALayer
 from .optimizer import FNAOptimizer
 from .injection import inject_fna_adapters, get_fna_optimizer_params, print_model_summary
+from .memory_layer import FNAMemoryLayer, inject_fna_memory
+
+__all__ = [
+    "advect", "diffuse", "project", "ns_step",
+    "FNALayer", "FNAOptimizer",
+    "inject_fna_adapters", "get_fna_optimizer_params", "print_model_summary",
+]
